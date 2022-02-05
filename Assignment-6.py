27ce98c1d35bebccc421109776da903c390cc91f
Mr-Rk001 <98808028+Mr-Rk001@users.noreply.github.com>
2022-02-05 23:02:43 +0530

    Day-6

---
+++ Assignment-6.py
@@ -0,0 +1,18 @@
+# Day 6 Assignment : Floor divison of any two number by using Decorators.
+
+
+def mod_div(fun):
+	def deno(a, b):
+		if a < b :
+			a, b = b, a
+		return fun(a, b)
+	return deno
+	
+@mod_div
+def div(a, b):
+	return  a // b
+
+a, b = (int(i) for i in input("Enter two Nunbers: ").split())
+
+
+print(div(a, b))
